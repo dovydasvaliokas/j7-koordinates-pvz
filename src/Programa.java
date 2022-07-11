61c57747062d0fd6f2b081585257dc555aaa172a
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 12:43:54 +0300

    Isbandau koordinates pagr. programoje

--- src/Programa.java
+++ src/Programa.java
@@ -1,2 +1,11 @@
 public class Programa {
+    public static void main(String[] args) {
+        Koordinate k1 = new Koordinate(5, 9);
+        Koordinate k2 = new Koordinate();
+        k2.setX(10);
+        k2.setY(8);
+
+        System.out.println("k2 = " + k2);
+        System.out.println("k1 = " + k1);
+    }
 }
