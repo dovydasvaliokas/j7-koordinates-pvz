b00404fa8e5857d5fa2baaa07364d2e620b3bffc
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 12:42:33 +0300

    Koordinates kintamieji, konstruktoriai, getteriai-setteriai

--- src/Koordinate.java
+++ src/Koordinate.java
@@ -1,3 +1,39 @@
 public class Koordinate {
+    private double x;
+    private double y;
 
+    public Koordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Koordinate() {
+    }
+
+
+
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Koordinate{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
